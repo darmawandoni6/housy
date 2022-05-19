1063e15e2b1ec099210e70e8f396bd42c93ca4f5
darmawandoni6 <doni.darmawan@amartha.com>
2022-05-19 22:04:44 +0700

    upload bukti

--- be/src/controllers/file/index.js
+++ be/src/controllers/file/index.js
@@ -1,7 +1,8 @@
 const formidable = require("formidable");
 const httpError = require("http-errors");
 const response = require("../../helpers/response");
-const { createProfile } = require("./service");
+const { findByid } = require("../booking/service");
+const { createProfile, updateBuktiPayment } = require("./service");
 
 const handleRollback = (name, option) => {
   const { path, files } = option;
@@ -53,6 +54,63 @@ module.exports = {
               id: data.id,
               fileUrl: process.env.BASE_URL + "/profile/" + newFilename,
             },
+          })
+        );
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  uploadFileBooking: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      let booking = { data: null, error: null };
+      booking = await findByid({ id, status: "Waiting Payment" });
+      if (booking.error) throw httpError.BadRequest(booking.error);
+
+      const path = "./public/booking";
+      const form = formidable({
+        multiples: false,
+        keepExtensions: true,
+        uploadDir: path,
+        maxFileSize: 10485760,
+      });
+
+      await form.parse(req, async (err, fields, files) => {
+        if (err) {
+          return next(httpError(err.httpCode, err.message));
+        }
+
+        if (Object.entries(files).length > 1)
+          return next(createHttpError.BadRequest("variable image only 1"));
+
+        let fileImage = {};
+        let nameFormData;
+        if (files["booking"]) {
+          nameFormData = "booking";
+          fileImage = await files["booking"];
+        } else {
+          return next(createHttpError.NotFound("variable image not found"));
+        }
+        const { newFilename, mimetype } = fileImage;
+
+        const { error } = await updateBuktiPayment(
+          {
+            name: newFilename,
+            type: mimetype,
+            folder: nameFormData,
+          },
+          booking.data
+        );
+        if (error) {
+          const err = await handleRollback(nameFormData, { path, files });
+          if (err) return next(err);
+          return next(httpError.BadRequest(error));
+        }
+        return res.status(200).json(
+          response.success({
+            message: "upload bukti transaksi success",
           })
         );
       });
--- be/src/routes/file.route.js
+++ be/src/routes/file.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { uploadFile } = require("../controllers/file");
+const { uploadFile, uploadFileBooking } = require("../controllers/file");
+const jwt = require("../helpers/jwt");
+
 const route = express.Router();
 
-route.post("/file", uploadFile);
+route.post("/file", jwt.verifyAccessToken, uploadFile);
+route.post("/file-booking/:id", jwt.verifyAccessToken, uploadFileBooking);
 
 module.exports = route;
